1f364f3dfbd072284e6ca9a95107d3c3b4a3f19c
Ivan <ivanitit3535@gmail.com>
2026-04-17 22:46:50 +0300

    feat: add vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+import { imageOptimizerPlugin } from './vite-plugins/image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), imageOptimizerPlugin()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/shared/assets', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/app/styles', import.meta.url)),
+      '@helpers': fileURLToPath(new URL('./src/app/styles/helpers', import.meta.url)),
+    },
+  },
+});
